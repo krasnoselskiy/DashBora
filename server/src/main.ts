0810d80e68d1679cb0548ac03e56f179733da29f
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-14 15:31:06 +0300

    Server: updated cors; added prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 import config from './config/env';
 
 async function bootstrap() {
   const port = config.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
   await app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`)
   });
