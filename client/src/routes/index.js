b8070bf1dbebfc7e6b5fd086adeb2b00af91690d
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-14 15:30:03 +0300

    Client: changed routes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,37 +2,44 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import styled from '@emotion/styled'
+import { connect } from 'react-redux';
 
 import Login from '../components/Login'
 import Register from '../components/Register';
+import Main from '../containers/Main';
+import Forbidden from '../components/Forbidden';
+import LayoutsSwitcher from '../components/LayoutsSwitcher';
 
 import 'antd/dist/antd.css';
 import 'ant-design-pro/dist/ant-design-pro.css';
-import Forbidden from '../components/Forbidden';
 
 const Container = styled.div`
   text-align: center;
 `
 export const history = createBrowserHistory()
 
-function Routes() {
+function Routes({ isLogin }) {
   return (
     <Router history={history}>
       <Container>
         <Switch>
-          <Route path="/register">
-            <Register />
+          <Route exact path='/'>
+            <LayoutsSwitcher isLoggedIn={isLogin} />
           </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-          <Route>
-            <Forbidden />
-          </Route>
+          <Route path='/home' component={Main} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route component={Forbidden} />
         </Switch>
       </Container>
     </Router>
   )
 }
 
-export default Routes
+const mapStateToProps = state => {
+  return {
+    isLogin: state.user.isLogin
+  }
+};
+
+export default connect(mapStateToProps)(Routes);
