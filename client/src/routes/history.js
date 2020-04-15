b38bdc2191b11027adbc64ea868eb6181750d73c
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-15 15:06:21 +0300

    Client: updated history for router

--- client/src/routes/history.js
+++ client/src/routes/history.js
@@ -1,3 +1,3 @@
-import { createBrowserHistory as createHistory } from 'history'
+import { createBrowserHistory } from 'history'
 
-export default createHistory()
+export default createBrowserHistory()
