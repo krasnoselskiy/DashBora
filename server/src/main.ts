f2f79bed76094a80a9065694cbfb7fd5b947382b
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-17 00:17:51 +0300

    Added swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import config from './config/env';
 
 async function bootstrap() {
   const port = config.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('FamilyBoard')
+    .setDescription('API for FamilyBoard App')
+    .setVersion('1.0')
+    .addTag('FamilyBoard')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
   app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`)
