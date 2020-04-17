6844380f01ac3511d7baf593701900b8aec8d0d7
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-17 20:47:06 +0300

    Moved auth module to components folder

---
+++ server/src/components/auth/auth.controller.ts
@@ -0,0 +1,46 @@
+import { AuthService } from './auth.service';
+import { UserService } from '../../shared/user.service';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) { }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  tempAUth() {
+    return { auth: 'works' }
+  }
+
+  @Post('login')
+  async login(@Body() userDTO: any) {
+    const user = await this.userService.findByLogin(userDTO);
+
+    const payload = {
+      username: user.username,
+      isSuperAdmin: user.isSuperAdmin
+    }
+
+    const token = await this.authService.signPayload(payload);
+
+    return { user, token };
+  }
+
+  @Post('register')
+  async register(@Body() userDTO: any) {
+    const user = await this.userService.create(userDTO);
+
+    const payload = {
+      username: user.username,
+      isSuperAdmin: user.isSuperAdmin
+    }
+
+    const token = await this.authService.signPayload(payload);
+
+    return { user, token };
+  }
+}
