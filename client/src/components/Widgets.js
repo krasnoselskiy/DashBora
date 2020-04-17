c1ba9ab3904587d9f49668807d62acd942df75f2
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-17 20:46:28 +0300

    Fetch widgets

--- client/src/components/Widgets.js
+++ client/src/components/Widgets.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 const Widgets = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get('http://localhost:3001/api/v1/widgets');
+      console.log(res);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       Widgets
