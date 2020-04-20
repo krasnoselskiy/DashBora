0f8feabce530a380d4b50928843e53fc574454f1
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-20 16:20:34 +0300

    Fixed styles

--- client/src/components/WidgetItem.js
+++ client/src/components/WidgetItem.js
@@ -5,9 +5,9 @@ import * as moment from 'moment';
 const WidgetItem = (props) => {
   const { name, date } = props.item;
   return (
-    <Card title={name}
+    <Card
+      title={name}
       extra={<Button type="primary">Add!</Button>}
-      style={{ width: "300px", marginRight: "15px" }}
     >
       <p>{moment(date).format('MM.DD.YYYY')}</p>
     </Card>
--- client/src/containers/Widgets.js
+++ client/src/containers/Widgets.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import styled from '@emotion/styled';
+import { Col, Row } from 'antd';
 
 import { widgetsFetch } from '../actions/widgets';
 import Preloader from '../components/Preloader';
@@ -16,20 +16,25 @@ const Widgets = (props) => {
   }, [widgets.length]);
 
   return (
-    <>
+    <React.Fragment>
       {loading && Preloader ?
         <Preloader /> : null
       }
       {error && FetchError ?
         <FetchError /> : null
       }
-      <WidgetList>
-        {widgets && WidgetItem ?
-          widgets.map((item, i) => (
-            <WidgetItem key={item._id} item={item} />
-          )) : null}
-      </WidgetList>
-    </>
+
+      <Row
+        gutter={16}
+        style={{ marginTop: "15px" }}>
+          {widgets && WidgetItem ?
+            widgets.map((item, i) => (
+              <Col span={8}>
+                <WidgetItem key={item._id} item={item} />
+              </Col>
+            )) : null}
+      </Row>
+    </React.Fragment>
   );
 }
 
@@ -46,12 +51,5 @@ const mapStateToProps = (state) => {
     error: state.widgets.error,
   };
 }
-
-const WidgetList = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  margin-top: 15px;
-`
 
 export default connect(mapStateToProps, mapDispatchToProps)(Widgets);
