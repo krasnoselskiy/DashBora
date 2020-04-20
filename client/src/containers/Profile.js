a8a575daf36db1928e8c8dfacfd939cb01c551ef
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-20 16:44:30 +0300

    Added description

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Descriptions } from 'antd';
 
 const Profile = () => {
   return (
-    <div>
-      Profile
-    </div>
+    <Descriptions title="User Info" styles={{ marginTop: "10px"}}>
+      <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+      <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+      <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+    </Descriptions>
   );
 }
 
