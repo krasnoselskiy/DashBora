0176ea92b495ad5e03eae00ad6e0366f7207a4a1
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-24 11:36:26 +0300

    Updater logger options

--- client/src/store/storeDev.js
+++ client/src/store/storeDev.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState = {}) {
+  const logger = createLogger({
+    collapsed: (getState, action, logEntry) => !logEntry.error
+  });
   const middlewares = [ReduxThunk, logger]
   const enhancers = [
     applyMiddleware(...middlewares),
