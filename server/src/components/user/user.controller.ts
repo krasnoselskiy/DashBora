2730c13f8b8c7548100e574c89704f6ffc4592cd
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-24 18:03:33 +0300

    Added to user widgets and get it

--- server/src/components/user/user.controller.ts
+++ server/src/components/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, Param, HttpStatus, Put, Body, Query, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Res, Param, HttpStatus, Put, Body, Query, NotFoundException, Post } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { ValidateObjectId } from '../shared/pipes/validate-object-id.pipes';
 import { User } from './interfaces/user.interface';
@@ -8,17 +8,66 @@ import { UserDTO } from './dto/user.dto';
 export class UserController {
   constructor(
     private userService: UserService,
+    // private widgetsService: WidgetsService,
   ) {
   }
 
-  @Get('/:userID')
-  // @UseGuards(AuthGuard('jwt'))
+  @Get('/:userId')
   async findByID(
     @Res() res,
-    @Param('userID', new ValidateObjectId()) userID
+    @Param('userId', new ValidateObjectId()) userId
   ) {
-    const user = await this.userService.findUserById(userID);
+    const user = await this.userService.findUserById(userId);
     return res.status(HttpStatus.OK).json(user);
+  }
+
+  @Get('/:userId/widgets')
+  async findUserWidgets(
+    @Res() res,
+    @Param('userId', new ValidateObjectId()) userId
+  ) {
+    const user = await this.userService.findUserById(userId);
+    return res.status(HttpStatus.OK).json(user.personalWidgets);
+  }
+
+  @Post('/addWidget')
+  async addWidget(
+    @Res() res,
+    @Body() body
+  ) {
+    const { userId, widgetId } = body;
+    const updatedUser = await this.userService.addProperty(
+      userId,
+      widgetId,
+      'personalWidgets'
+    );
+
+    if (!updatedUser) throw new NotFoundException('User does not exist!');
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Widget has been successfully added',
+      user: updatedUser
+    })
+  }
+
+  @Post('/removeWidget')
+  async removeWidget(
+    @Res() res,
+    @Body() body
+  ) {
+    const { userId, widgetId } = body;
+    const updatedUser = await this.userService.removeProperty(
+      userId,
+      widgetId,
+      'personalWidgets'
+    );
+
+    if (!updatedUser) throw new NotFoundException('User does not exist!');
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Widget has been successfully removed',
+      user: updatedUser
+    })
   }
 
   @Put('/update/:userID')
