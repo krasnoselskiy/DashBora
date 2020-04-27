a11488ccce59eda0d4bb5f4afec45daee747dc36
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-04-27 19:32:31 +0300

    Update team creation

--- server/src/components/teams/teams.controller.ts
+++ server/src/components/teams/teams.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, NotFoundException, HttpStatus, Res } from '@nestjs/common';
 import { Team } from './interfaces/team.interface';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { TeamsService } from './teams.service';
@@ -15,12 +15,18 @@ export class TeamsController {
     return this.teamsService.findAll();
   }
 
-  @Post()
-  async create(@Body() createTeamDto: CreateTeamDto) {
-    await this.teamsService.create(createTeamDto);
+  @Post('/create')
+  async create(
+    @Res() res,
+    @Body() createTeamDto: CreateTeamDto
+  ) {
+    const team =  await this.teamsService.create(createTeamDto);
 
-    return {
-      "result": "success"
-    };
+    if (!team) throw new NotFoundException('Cant create this team!');
+
+    return res.status(HttpStatus.OK).json({
+      message: 'Team has been successfully created',
+      team: team
+    })
   }
 }
